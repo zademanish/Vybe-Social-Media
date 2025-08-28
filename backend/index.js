16730adf5e955ea00a1b720479d65f165991ce1c
manish <manishzademz7@gmail.com>
2025-08-28 17:43:08 +0530

    add deployed frontend url

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ dotenv.config()
 
 const port =process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://vybe-frontend-x5it.onrender.com",
    credentials:true
 }))
 
